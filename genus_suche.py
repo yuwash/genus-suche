b62b447550d77dcb83ea3688e7ad027c1cd170e0
Yushin Washio <yuwash@yandex.com>
2020-01-01 20:59:52 +0100

    add doctest to print_genus

--- genus_suche.py
+++ genus_suche.py
@@ -6,7 +6,7 @@ import slob
 
 def iter_gen(keyditemdict):
     for i in range(len(keyditemdict)):
-        item = d.lst[i]  # slob.Blob
+        item = keyditemdict.lst[i]  # slob.Blob
         soup = BeautifulSoup(item.content, 'html.parser')
         if soup.body is None:
             continue
@@ -19,9 +19,34 @@ def iter_gen(keyditemdict):
         yield item, soup_gen.string
 
 
+def print_genus(keyditemdict, limit=50):
+    """
+    >>> import os.path
+    >>> from tempfile import TemporaryDirectory
+    >>> data = {'Erde': 'f', 'Planet': 'm', 'Gestein': 'n'}
+    >>> content_template = (
+    ...     '<html><head></head><body><div></div>'
+    ...     '<div class="gramGrp"><div></div><div class="gen">{}</div></div>'
+    ...     '</body></html>'
+    ... )
+    >>> with TemporaryDirectory() as dirname:
+    ...     path = os.path.join(dirname, 'example.slob')
+    ...     with slob.create(path) as w:
+    ...         for word, gender in data.items():
+    ...             w.add(
+    ...                 content_template.format(gender).encode('utf-8'), word)
+    ...     with slob.open(path) as r:
+    ...         print_genus(r.as_dict())
+    Erde (f)
+    Gestein (n)
+    Planet (m)
+    """
+    for item, gen in itertools.islice(iter_gen(keyditemdict), limit):
+        print('{} ({})'.format(item.key, gen))
+
+
 if __name__ == '__main__':
     # from https://download.freedict.org/dictionaries/deu-eng/0.3.5/freedict-deu-eng-0.3.5.slob
     with slob.open('freedict-deu-eng-0.3.5.slob') as r:
         d = r.as_dict()  # slob.KeydItemDict
-        for item, gen in itertools.islice(iter_gen(d), 50):
-            print('{} ({})'.format(item.key, gen))
+        print_genus(d)
